3debf22056a292530d8ff2b4b7529396509645a5
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-11-15 15:45:00 +0530

    .

--- assets/js/apis/dashboard.js
+++ assets/js/apis/dashboard.js
@@ -22,3 +22,18 @@ const getCategories = async () => {
 
 // Call the function to fetch categories
 getCategories();
+const getBooks = async () => {
+    try {
+        let response = await fetch('https://pustakam.pythonanywhere.com/books/');
+        let data = await response.json();
+        let lengthOfCategories = data.data.length;
+
+        // Assuming you have an element with ID `dashboardCategoriesTotal` to display the count
+        document.getElementById('dashboardBooksTotal').innerHTML = lengthOfCategories;
+    } catch (error) {
+        console.error('Error fetching categories:', error);
+    }
+}
+
+// Call the function to fetch categories
+getBooks();
